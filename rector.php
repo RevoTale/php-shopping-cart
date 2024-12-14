d4ebb1e1ef9a3c3fddc3a36c6ebba614a74207c7
l-you <l-you@revotale.com>
2024-12-14 11:38:49 +0200

    update rector config

--- rector.php
+++ rector.php
@@ -9,9 +9,7 @@ return RectorConfig::configure()
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
-    ->withTypeCoverageLevel(0)
-    ->withDeadCodeLevel(0)
     ->withPreparedSets(
-        deadCode: true, codeQuality: true, codingStyle: true, typeDeclarations: true, privatization: true, naming: true, instanceOf: true, strictBooleans: true, rectorPreset: true, phpunitCodeQuality: true
+        deadCode: true, codeQuality: true, codingStyle: true, typeDeclarations: true, privatization: true, instanceOf: true, strictBooleans: true, rectorPreset: true, phpunitCodeQuality: true
     )
-    ->withCodeQualityLevel(0);
+    ;
